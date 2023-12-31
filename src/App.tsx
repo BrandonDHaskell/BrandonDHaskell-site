821808b4cd70bb1c7e366a08e01bacfaa1ec8eeb
BrandonDHaskell <BrandonDHaskell@outlook.com>
2023-12-30 20:47:31 -0800

    add style to profile

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,22 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h5>Hi, my name is</h5>
-                <h1>BrandonDHaskell</h1>
-                <img id="round-corners-profile" src="/Profile_Pic_2_medium.jpg" alt="Profile" />
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div className="grid grid-cols-2 gap-4 bg-md-blue">
+                    <div className="grid justify-items-end content-center gap-4">
+                        <h5 className="pl-2">Hi, my name is</h5>
+                        <h1 className="pl-2">BrandonDHaskell</h1>
+                        <p className="pl-2 text-right">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <div>
+                        <img
+                            id="round-corners-profile"
+                            src="/Profile_Pic_2_medium.jpg"
+                            alt="Profile"
+                            className="px-6"
+                            style={{ maxWidth: '100%', height: 'auto', objectFit: 'contain' }}
+                        />
+                    </div>
+                </div>
             </header>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,css,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gm-grey": "#1c1d23",
+        "md-blue": "#0d0d38",
+      },
+    },
   },
   plugins: [],
 };
